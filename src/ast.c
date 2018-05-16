fa8400e389b2b4160c868e45390fe25311008866
Ronak Chauhan <ronakchauhan97@gmail.com>
2018-05-16 16:04:49 +0530

    Changes to AST node definition
    - Added separate type for nodes in AST

--- src/ast.c
+++ src/ast.c
@@ -9,13 +9,156 @@
 #include <stdlib.h>
 #include "ast.h"
 
+static size_t global_node_count = 0;
+
+
+static ast_label_t get_label(token_t token)
+{
+	switch (token.symbol) {
+		case VAR: return AST_VAR_DECL;
+		case CONST: return AST_CONST_DECL;
+		case PROCEDURE: return AST_PROC_DECL;
+		case CALL: return AST_CALL;
+		case PLUS: return AST_ADD;
+		case MINUS: return AST_SUB;
+		case TIMES: return AST_MUL;
+		case SLASH: return AST_DIV;
+		case ODD: return AST_ODD;
+		case ASSIGN: return AST_ASSIGN;
+		case GTE: return AST_GTE;
+		case LTE: return AST_LTE;
+		case GREATER: return AST_GT;
+		case LESSER: return AST_LT;
+		case EQUAL: return AST_EQ;
+		case NOTEQUAL: return AST_NEQ;
+		case NUM: return AST_NUM;
+		case IDENT: return AST_IDENT;
+		case PRINT: return AST_PRINT;
+		case SCAN: return AST_SCAN;
+		case BEGIN: return AST_STMT_BLOCK;
+		case IF: return AST_IF;
+		case WHILE: return AST_WHILE;
+	}
+}
+
+
+static void print_ast_label(ast_label_t label)
+{
+	switch(label) {
+		case AST_ROOT:
+		printf("AST_ROOT\n");
+		break;
+		
+		case AST_VAR_DECL:
+		printf("AST_VAR_DECL\n");
+		break;
+	
+		case AST_CONST_DECL:
+		printf("AST_CONST_DECL\n");
+		break;
+	
+		case AST_PROC_DECL:
+		printf("AST_PROC_DECL\n");
+		break;
+	
+		case AST_CALL:
+		printf("AST_CALL\n");
+		break;
+	
+		case AST_ADD:
+		printf("AST_ADD\n");
+		break;
+	
+		case AST_SUB:
+		printf("AST_SUB\n");
+		break;
+	
+		case AST_MUL:
+		printf("AST_MUL\n");
+		break;
+	
+		case AST_DIV:
+		printf("AST_DIV\n");
+		break;
+	
+		case AST_ASSIGN:
+		printf("AST_ASSIGN\n");
+		break;
+	
+		case AST_ODD:
+		printf("AST_ODD\n");
+		break;
+	
+		case AST_GTE:
+		printf("AST_GTE\n");
+		break;
+	
+		case AST_LTE:
+		printf("AST_LTE\n");
+		break;
+	
+		case AST_GT:
+		printf("AST_GT\n");
+		break;
+	
+		case AST_LT:
+		printf("AST_LT\n");
+		break;
+	
+		case AST_EQ:
+		printf("AST_EQ\n");
+		break;
+	
+		case AST_NEQ:
+		printf("AST_NEQ\n");
+		break;
+	
+		case AST_NUM:
+		printf("AST_NUM\n");
+		break;
+	
+		case AST_IDENT:
+		printf("AST_IDENT\n");
+		break;
+	
+		case AST_PRINT:
+		printf("AST_PRINT\n");
+		break;
+	
+		case AST_SCAN:
+		printf("AST_SCAN\n");
+		break;
+	
+		case AST_BLOCK:
+		printf("AST_BLOCK\n");
+		break;
+	
+		case AST_STMT_BLOCK:
+		printf("AST_STMT_BLOCK\n");
+		break;
+	
+		case AST_IF:
+		printf("AST_IF\n");
+		break;
+	
+		case AST_WHILE:
+		printf("AST_WHILE\n");
+		break;
+	
+		default:
+		printf("ERROR\n");
+		break;
+	}
+}
+
 
 ast_node_t *new_ast_node(token_t token)
 {
 	ast_node_t *new_node = calloc(sizeof(ast_node_t), 1);
-	new_node->token = token;
+	new_node->label = get_label(token);
 	new_node->first_child = NULL;
 	new_node->next_sibling = NULL;
+	global_node_count++;
 	return new_node;
 }
 
@@ -38,13 +181,13 @@ void append_child(ast_node_t *parent, ast_node_t *new_child)
 
 void print_ast_node(ast_node_t *node)
 {
-	printf("node token:\n");
-	print_token(node->token);
+	print_ast_label(node->label);
+	// todo: add code to print identifiers and their details (scope, value, etc);
 	printf("\n\n");
 }
 
 
-/* Printing AST nodes in preorder sequence */
+// Printing AST nodes in preorder sequence
 void print_ast(ast_node_t *root)
 {
 	if (!root) {
@@ -58,4 +201,10 @@ void print_ast(ast_node_t *root)
 			c_root = c_root->next_sibling;
 		}
 	}
+}
+
+
+size_t ast_node_count()
+{
+	return global_node_count;
 }
--- src/ast.h
+++ src/ast.h
@@ -10,8 +10,26 @@
 
 #include "token.h"
 
+typedef enum {
+	AST_ROOT,
+	
+	AST_VAR_DECL, AST_CONST_DECL, AST_PROC_DECL, AST_CALL,
+	
+	AST_ADD, AST_SUB, AST_MUL, AST_DIV, AST_ASSIGN, AST_ODD,
+	AST_GTE, AST_LTE, AST_GT, AST_LT, AST_EQ, AST_NEQ,
+	
+	AST_NUM, AST_IDENT,
+	AST_PRINT, AST_SCAN,
+
+	AST_BLOCK, AST_STMT_BLOCK,
+	AST_IF, AST_WHILE
+
+} ast_label_t;
+
+
 typedef struct ast_node {
-	token_t token;
+	ast_label_t label;
+	int num_value;
 	struct ast_node *first_child;
 	struct ast_node *next_sibling;
 } ast_node_t;
@@ -24,5 +42,7 @@ void append_child(ast_node_t *parent, ast_node_t *new_child);
 void print_ast_node(ast_node_t *node);
 
 void print_ast(ast_node_t *root);
+
+size_t ast_node_count();
 
 #endif
